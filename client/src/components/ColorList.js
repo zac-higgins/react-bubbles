85dac9c01027ae174a46a4e849349b206482008c
Zac Higgins <zachiggins@Higgins-iMac.local>
2019-11-15 10:24:57 -0700

    added edit and delete functionality to color list

--- client/src/components/ColorList.js
+++ client/src/components/ColorList.js
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import axios from "axios";
+import axiosWithAuth from './axiosWithAuth';
 
 const initialColor = {
   color: "",
@@ -21,10 +22,14 @@ const ColorList = ({ colors, updateColors }) => {
     // Make a put request to save your updated color
     // think about where will you get the id from...
     // where is is saved right now?
+    axiosWithAuth()
+      .put(`http://localhost:5000/api/colors/${colorToEdit.id}`, colorToEdit)
   };
 
   const deleteColor = color => {
     // make a delete request to delete this color
+    axiosWithAuth()
+      .delete(`http://localhost:5000/api/colors/${color.id}`)
   };
 
   return (
